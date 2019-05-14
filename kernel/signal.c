781fc09f3d90b25d95e623e08b7f31edd548d76f
Mathieu Malaterre <malat@debian.org>
2019-05-14 15:44:35 -0700

    kernel/signal.c: annotate implicit fall through
    There is a plan to build the kernel with -Wimplicit-fallthrough and this
    place in the code produced a warning (W=1).
    
    This commit remove the following warning:
    
      kernel/signal.c:795:13: warning: this statement may fall through [-Wimplicit-fallthrough=]
    
    Link: http://lkml.kernel.org/r/20190114203505.17875-1-malat@debian.org
    Signed-off-by: Mathieu Malaterre <malat@debian.org>
    Acked-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -854,6 +854,7 @@ static int check_kill_permission(int sig, struct siginfo *info,
 			 */
 			if (!sid || sid == task_session(current))
 				break;
+			/* fall through */
 		default:
 			return -EPERM;
 		}
