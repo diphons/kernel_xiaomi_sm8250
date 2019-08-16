67763c69c6128e824a7ff2fe8ffb8b445fca24a3
Thomas Gleixner <tglx@linutronix.de>
2019-08-16 18:09:23 +0200

    genirq: Force interrupt threading on RT
    Switch force_irqthreads from a boot time modifiable variable to a compile
    time constant when CONFIG_PREEMPT_RT is enabled.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20190816160923.12855-1-bigeasy@linutronix.de

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -32,7 +32,7 @@ static int prime_cpu_index = -1;
 static int lp_cpu_index = -1;
 static bool perf_crit_suspended;
 
-#ifdef CONFIG_IRQ_FORCED_THREADING
+#if defined(CONFIG_IRQ_FORCED_THREADING) && !defined(CONFIG_PREEMPT_RT)
 __read_mostly bool force_irqthreads;
 EXPORT_SYMBOL_GPL(force_irqthreads);
 
