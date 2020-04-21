3ba115242d08d0fe97cbff578e3c376e529ad863
Chen Yu <yu.c.chen@intel.com>
2020-04-21 18:50:34 +0800

    sched: Make newidle_balance() static again
    After Commit 6e2df0581f56 ("sched: Fix pick_next_task() vs 'change'
    pattern race"), there is no need to expose newidle_balance() as it
    is only used within fair.c file. Change this function back to static again.
    
    No functional change.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/83cd3030b031ca5d646cd5e225be10e7a0fdd8f5.1587464698.git.yu.c.chen@intel.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1680,8 +1680,6 @@ static inline void unregister_sched_domain_sysctl(void)
 }
 #endif
 
-extern int newidle_balance(struct rq *this_rq, struct rq_flags *rf);
-
 static inline const struct cpumask *task_user_cpus(struct task_struct *p)
 {
 	if (!p->user_cpus_ptr)
@@ -1691,7 +1689,6 @@ static inline const struct cpumask *task_user_cpus(struct task_struct *p)
 #else
 
 static inline void sched_ttwu_pending(void) { }
-static inline int newidle_balance(struct rq *this_rq, struct rq_flags *rf) { return 0; }
 
 #endif /* CONFIG_SMP */
 
