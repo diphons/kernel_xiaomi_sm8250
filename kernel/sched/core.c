d93702c12b94afc9acbbfdfeeeac5aa114ca1380
Scott Wood <swood@redhat.com>
2020-06-17 14:17:42 +0200

    sched/core: Check cpus_mask, not cpus_ptr in __set_cpus_allowed_ptr()
    to fix mask corruption.
    
    This function is concerned with the long-term CPU mask, not the
    transitory mask the task might have while migrate disabled.  Before
    this patch, if a task was migrate-disabled at the time
    __set_cpus_allowed_ptr() was called, and the new mask happened to be
    equal to the CPU that the task was running on, then the mask update
    would be lost.
    
    Signed-off-by: Scott Wood <swood@redhat.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200617121742.cpxppyi7twxmpin7@linutronix.de

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1969,7 +1969,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 		goto out;
 	}
 
-	if (cpumask_equal(p->cpus_ptr, new_mask))
+	if (cpumask_equal(&p->cpus_mask, new_mask))
 		goto out;
 	dest_cpu = cpumask_any_and(cpu_valid_mask, new_mask);
 	if (dest_cpu >= nr_cpu_ids) {
