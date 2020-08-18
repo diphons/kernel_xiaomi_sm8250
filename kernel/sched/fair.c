d5409782acb817c4944c4509e28b0acb2181b48e
Peter Zijlstra <peterz@infradead.org>
2020-08-18 10:48:17 +0200

    sched/fair: Exclude the current CPU from find_new_ilb()
    It is possible for find_new_ilb() to select the current CPU, however,
    this only happens from newidle balancing, in which case need_resched()
    will be true, and consequently nohz_csd_func() will not trigger the
    softirq.
    
    Exclude the current CPU from becoming an ILB target.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11316,6 +11316,10 @@ static inline int find_new_ilb(void)
 	hk_mask = housekeeping_cpumask(HK_TYPE_MISC);
 
 	for_each_cpu_and(ilb, nohz.idle_cpus_mask, hk_mask) {
+		
+		if (ilb == smp_processor_id())
+			continue;
+
 		if (idle_cpu(ilb))
 			return ilb;
 	}
