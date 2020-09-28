0fb364f01c713ad990af9f3f67feea94b5f8e94c
Peter Zijlstra <peterz@infradead.org>
2020-09-28 17:06:07 +0200

    sched: Fix migrate_disable() vs rt/dl balancing
    In order to minimize the interference of migrate_disable() on lower
    priority tasks, which can be deprived of runtime due to being stuck
    below a higher priority task. Teach the RT/DL balancers to push away
    these higher priority tasks when a lower priority task gets selected
    to run on a freshly demoted CPU (pull).
    
    This adds migration interference to the higher priority task, but
    restores bandwidth to system that would otherwise be irrevocably lost.
    Without this it would be possible to have all tasks on the system
    stuck on a single CPU, each task preempted in a migrate_disable()
    section with a single high priority task running.
    
    This way we can still approximate running the M highest priority tasks
    on the system.
    
    Migrating the top task away is (ofcourse) still subject to
    migrate_disable() too, which means the lower task is subject to an
    interference equivalent to the worst case migrate_disable() section.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102347.499155098@infradead.org

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2725,6 +2725,9 @@ static int push_dl_task(struct rq *rq)
 		return 0;
 
 retry:
+	if (is_migration_disabled(next_task))
+		return 0;
+
 	if (WARN_ON(next_task == rq->curr))
 		return 0;
 
@@ -2808,7 +2811,7 @@ static void push_dl_tasks(struct rq *rq)
 static void pull_dl_task(struct rq *this_rq)
 {
 	int this_cpu = this_rq->cpu, cpu;
-	struct task_struct *p;
+	struct task_struct *p, *push_task;
 	bool resched = false;
 	struct rq *src_rq;
 	u64 dmin = LONG_MAX;
@@ -2838,6 +2841,7 @@ static void pull_dl_task(struct rq *this_rq)
 			continue;
 
 		/* Might drop this_rq->lock */
+		push_task = NULL;
 		double_lock_balance(this_rq, src_rq);
 
 		/*
@@ -2867,23 +2871,33 @@ static void pull_dl_task(struct rq *this_rq)
 					   src_rq->curr->dl.deadline))
 				goto skip;
 
-			resched = true;
-
-			deactivate_task(src_rq, p, 0);
-			sub_running_bw(&p->dl, &src_rq->dl);
-			sub_rq_bw(&p->dl, &src_rq->dl);
-			p->on_rq = TASK_ON_RQ_MIGRATING;
-			set_task_cpu(p, this_cpu);
-			p->on_rq = TASK_ON_RQ_QUEUED;
-			add_rq_bw(&p->dl, &this_rq->dl);
-			add_running_bw(&p->dl, &this_rq->dl);
-			activate_task(this_rq, p, 0);
-			dmin = p->dl.deadline;
+			if (is_migration_disabled(p)) {
+				push_task = get_push_task(src_rq);
+			} else {
+				deactivate_task(src_rq, p, 0);
+				sub_running_bw(&p->dl, &src_rq->dl);
+				sub_rq_bw(&p->dl, &src_rq->dl);
+				p->on_rq = TASK_ON_RQ_MIGRATING;
+				set_task_cpu(p, this_cpu);
+				p->on_rq = TASK_ON_RQ_QUEUED;
+				add_rq_bw(&p->dl, &this_rq->dl);
+				add_running_bw(&p->dl, &this_rq->dl);
+				activate_task(this_rq, p, 0);
+				dmin = p->dl.deadline;
+				resched = true;
+			}
 
 			/* Is there any other task even earlier? */
 		}
 skip:
 		double_unlock_balance(this_rq, src_rq);
+
+		if (push_task) {
+			raw_spin_unlock(&this_rq->lock);
+			stop_one_cpu_nowait(src_rq->cpu, push_cpu_stop,
+					    push_task, &src_rq->push_work);
+			raw_spin_lock(&this_rq->lock);
+		}
 	}
 
 	if (resched)
@@ -3170,6 +3184,7 @@ const struct sched_class dl_sched_class = {
 	.rq_online              = rq_online_dl,
 	.rq_offline             = rq_offline_dl,
 	.task_woken		= task_woken_dl,
+	.find_lock_rq		= find_lock_later_rq,
 #endif
 
 	.task_tick		= task_tick_dl,
