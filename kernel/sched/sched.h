fab2dda927db35919f767b4d8077978da50121ed
Peter Zijlstra <peterz@infradead.org>
2020-09-28 17:06:07 +0200

    sched: Fix migrate_disable() vs rt/dl balancing
    In order to minimize the interference of migrate_disable() on lower
    priority tasks, which can be deprived of runtime due to being stuck
    below a higher priority task. Teach the RT/DL balancers to push away
    these higher priority tasks when a lower priority task gets selected
    to run on a freshly demoted CPU (pull).
    
    This adds migration interference to the higher priority task, but
    restores bandwidth to system that would otherwise be irrevocably lost.
    Without this it would be possible to have all tasks on the system
    stuck on a single CPU, each task preempted in a migrate_disable()
    section with a single high priority task running.
    
    This way we can still approximate running the M highest priority tasks
    on the system.
    
    Migrating the top task away is (ofcourse) still subject to
    migrate_disable() too, which means the lower task is subject to an
    interference equivalent to the worst case migrate_disable() section.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102347.499155098@infradead.org

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1149,6 +1149,8 @@ struct rq {
 #if defined(CONFIG_PREEMPT_RT) && defined(CONFIG_SMP)
 	unsigned int		nr_pinned;
 #endif
+	unsigned int		push_busy;
+	struct cpu_stop_work	push_work;
 
 #ifdef CONFIG_SCHED_CORE
 	/* per rq */
@@ -1190,6 +1192,17 @@ static inline int cpu_of(struct rq *rq)
 	return rq->cpu;
 #else
 	return 0;
+#endif
+}
+
+#define MDF_PUSH	0x01
+
+static inline bool is_migration_disabled(struct task_struct *p)
+{
+#if defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT)
+	return p->migration_disabled;
+#else
+	return false;
 #endif
 }
 
@@ -2155,6 +2168,8 @@ struct sched_class {
 
 	void (*rq_online)(struct rq *rq);
 	void (*rq_offline)(struct rq *rq);
+
+	struct rq *(*find_lock_rq)(struct task_struct *p, struct rq *rq);
 #endif
 
 	void (*task_tick)(struct rq *rq, struct task_struct *p, int queued);
@@ -2276,6 +2291,24 @@ extern void set_cpus_allowed_common(struct task_struct *p, const struct cpumask 
 
 bool __cpu_overutilized(int cpu, int delta);
 bool cpu_overutilized(int cpu);
+
+static inline struct task_struct *get_push_task(struct rq *rq)
+{
+	struct task_struct *p = rq->curr;
+
+	lockdep_assert_rq_held(rq);
+
+	if (rq->push_busy)
+		return NULL;
+
+	if (p->nr_cpus_allowed == 1)
+		return NULL;
+
+	rq->push_busy = true;
+	return get_task_struct(p);
+}
+
+extern int push_cpu_stop(void *arg);
 
 #endif
 
