b141b7f834a56f3cdbcb70e043cb734e3d8329ee
Viresh Kumar <viresh.kumar@linaro.org>
2020-12-08 09:46:56 +0530

    sched/core: Rename schedutil_cpu_util() and allow rest of the kernel to use it
    There is nothing schedutil specific in schedutil_cpu_util(), rename it
    to effective_cpu_util(). Also create and expose another wrapper
    sched_cpu_util() which can be used by other parts of the kernel, like
    thermal core (that will be done in a later commit).
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lkml.kernel.org/r/db011961fb3bb8bef1c0eda5cd64564637d3ef31.1607400596.git.viresh.kumar@linaro.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5245,7 +5245,7 @@ struct task_struct *idle_task(int cpu)
  * based on the task model parameters and gives the minimal utilization
  * required to meet deadlines.
  */
-unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
+unsigned long effective_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long *min,
 				 unsigned long *max)
 {
@@ -5317,6 +5317,11 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	util += irq;
 
 	return min(scale, util);
+}
+
+unsigned long sched_cpu_util(int cpu, unsigned long max)
+{
+	return effective_cpu_util(cpu, cpu_util_cfs(cpu_rq(cpu)), NULL, &max);
 }
 #endif
 
