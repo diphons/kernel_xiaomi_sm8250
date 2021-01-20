1a3b3b3d97473d49d5b0388e2d0b4053fe95c450
Peter Zijlstra <peterz@infradead.org>
2021-01-20 15:05:41 +0100

    sched: Prepare to use balance_push in ttwu()
    In preparation of using the balance_push state in ttwu() we need it to
    provide a reliable and consistent state.
    
    The immediate problem is that rq->balance_callback gets cleared every
    schedule() and then re-set in the balance_push_callback() itself. This
    is not a reliable signal, so add a variable that stays set during the
    entire time.
    
    Also move setting it before the synchronize_rcu() in
    sched_cpu_deactivate(), such that we get guaranteed visibility to
    ttwu(), which is a preempt-disable region.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210121103506.966069627@infradead.org

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1054,6 +1054,7 @@ struct rq {
 	unsigned long		cpu_capacity;
 
 	struct callback_head	*balance_callback;
+	unsigned char		balance_push;
 	unsigned char		balance_flags;
 
 	unsigned char		idle_balance;
