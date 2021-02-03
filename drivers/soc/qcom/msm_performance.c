e581b9726369ab1c605cf3a2dd3a401933d7aee7
Pavankumar Kondeti <pkondeti@codeaurora.org>
2021-02-03 21:14:50 +0530

    sched/walt: Move public declarations of core_ctl.h to walt.h
    core_ctl is part of WALT. There is no separate config for
    core_ctl. So it makes sense to declare all the WALT related
    public interfaces in a single header file.
    
    Change-Id: I0ae5585c0037295eee4268705f0e553d14c60fd9
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -17,7 +17,7 @@
 #include <linux/module.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
-#include <linux/sched/core_ctl.h>
+#include <linux/sched/walt.h>
 
 /*
  * Sched will provide the data for every 20ms window,
