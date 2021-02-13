c55a7fd9f0c7ab9412dd91162cf444a86c4b681b
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2021-02-13 11:53:13 +0530

    ANDROID: sched: Export max_load_balance_interval
    Export max_load_balance_interval so that vendor modules can adjust
    the load balance interval.
    
    Bug: 180125905
    Change-Id: I9e5572db92747d17f9f1f7cd97725bbb04fc0e32
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8624,7 +8624,8 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *      rewrite all of this once again.]
  */
 
-static unsigned long __read_mostly max_load_balance_interval = HZ/10;
+unsigned long __read_mostly max_load_balance_interval = HZ/10;
+EXPORT_SYMBOL_GPL(max_load_balance_interval);
 
 enum fbq_type { regular, remote, all };
 
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1909,6 +1909,8 @@ extern void trigger_load_balance(struct rq *rq);
 
 extern void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_mask);
 
+extern unsigned long __read_mostly max_load_balance_interval;
+
 bool __cpu_overutilized(int cpu, int delta);
 bool cpu_overutilized(int cpu);
 
