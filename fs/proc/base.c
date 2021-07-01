ce84736d43da4bda32de723f197dcae5c51fe3c7
Kalesh Singh <kaleshsingh@google.com>
2021-06-30 18:54:44 -0700

    procfs: allow reading fdinfo with PTRACE_MODE_READ
    Android captures per-process system memory state when certain low memory
    events (e.g a foreground app kill) occur, to identify potential memory
    hoggers.  In order to measure how much memory a process actually consumes,
    it is necessary to include the DMA buffer sizes for that process in the
    memory accounting.  Since the handle to DMA buffers are raw FDs, it is
    important to be able to identify which processes have FD references to a
    DMA buffer.
    
    Currently, DMA buffer FDs can be accounted using /proc/<pid>/fd/* and
    /proc/<pid>/fdinfo -- both are only readable by the process owner, as
    follows:
    
      1. Do a readlink on each FD.
      2. If the target path begins with "/dmabuf", then the FD is a dmabuf FD.
      3. stat the file to get the dmabuf inode number.
      4. Read/ proc/<pid>/fdinfo/<fd>, to get the DMA buffer size.
    
    Accessing other processes' fdinfo requires root privileges.  This limits
    the use of the interface to debugging environments and is not suitable for
    production builds.  Granting root privileges even to a system process
    increases the attack surface and is highly undesirable.
    
    Since fdinfo doesn't permit reading process memory and manipulating
    process state, allow accessing fdinfo under PTRACE_MODE_READ_FSCRED.
    
    Link: https://lkml.kernel.org/r/20210308170651.919148-1-kaleshsingh@google.com
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Suggested-by: Jann Horn <jannh@google.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Alexey Gladkov <gladkov.alexey@gmail.com>
    Cc: Andrei Vagin <avagin@gmail.com>
    Cc: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Hridya Valsaraju <hridya@google.com>
    Cc: James Morris <jamorris@linux.microsoft.com>
    Cc: Jeff Vander Stoep <jeffv@google.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Szabolcs Nagy <szabolcs.nagy@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3832,7 +3832,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 	DIR("task",       0555, proc_task_inode_operations, proc_task_operations),
 	DIR("fd",         0500, proc_fd_inode_operations, proc_fd_operations),
 	DIR("map_files",  0500, proc_map_files_inode_operations, proc_map_files_operations),
-	DIR("fdinfo",     0500, proc_fdinfo_inode_operations, proc_fdinfo_operations),
+	DIR("fdinfo",     0555, proc_fdinfo_inode_operations, proc_fdinfo_operations),
 	DIR("ns",	  0511, proc_ns_dir_inode_operations, proc_ns_dir_operations),
 #ifdef CONFIG_NET
 	DIR("net",        0555, proc_net_inode_operations, proc_net_operations),
@@ -4256,7 +4256,7 @@ static const struct inode_operations proc_tid_comm_inode_operations = {
  */
 static const struct pid_entry tid_base_stuff[] = {
 	DIR("fd",        0500, proc_fd_inode_operations, proc_fd_operations),
-	DIR("fdinfo",    0500, proc_fdinfo_inode_operations, proc_fdinfo_operations),
+	DIR("fdinfo",    0555, proc_fdinfo_inode_operations, proc_fdinfo_operations),
 	DIR("ns",	 0511, proc_ns_dir_inode_operations, proc_ns_dir_operations),
 #ifdef CONFIG_NET
 	DIR("net",        0555, proc_net_inode_operations, proc_net_operations),
