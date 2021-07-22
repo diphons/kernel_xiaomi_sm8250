24b763aa76b332abac23dee5067c80b6bc72813f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-07-22 05:30:58 +0200

    input: touchscreen: nt36672c: Disable Debugging
    - This driver is working just fine and perfectly
      so please do not spam or report anything in dmesg
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I44ac2a51ae4f60f10f38453fdc2522c62e87471b
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.h
+++ drivers/input/touchscreen/nt36672c/nt36xxx.h
@@ -72,12 +72,8 @@
 /*---SPI driver info.---*/
 #define NVT_SPI_NAME "NVT-ts-spi"
 
-#if NVT_DEBUG
-#define NVT_LOG(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
-#else
-#define NVT_LOG(fmt, args...)    pr_info("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
-#endif
-#define NVT_ERR(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
+#define NVT_LOG(fmt, args...) ((void)0)
+#define NVT_ERR(fmt, args...) ((void)0)
 
 /*---Input device info.---*/
 #define NVT_TS_NAME "NVTCapacitiveTouchScreen"
