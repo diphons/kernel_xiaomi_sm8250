305daddd7fdcc459f1288522c126e0e333009310
Will Deacon <will@kernel.org>
2021-07-30 12:24:33 +0100

    sched: Introduce task_struct::user_cpus_ptr to track requested affinity
    In preparation for saving and restoring the user-requested CPU affinity
    mask of a task, add a new cpumask_t pointer to 'struct task_struct'.
    
    If the pointer is non-NULL, then the mask is copied across fork() and
    freed on task exit.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <Valentin.Schneider@arm.com>
    Link: https://lore.kernel.org/r/20210730112443.23245-7-will@kernel.org

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -868,6 +868,7 @@ struct task_struct {
 	unsigned int			policy;
 	int				nr_cpus_allowed;
 	const cpumask_t			*cpus_ptr;
+	cpumask_t			*user_cpus_ptr;
 	cpumask_t			cpus_mask;
 	cpumask_t			cpus_requested;
 	void				*migration_pending;
@@ -1816,6 +1817,8 @@ extern int task_can_attach(struct task_struct *p, const struct cpumask *cs_cpus_
 #ifdef CONFIG_SMP
 extern void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask);
 extern int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask);
+extern int dup_user_cpus_ptr(struct task_struct *dst, struct task_struct *src, int node);
+extern void release_user_cpus_ptr(struct task_struct *p);
 extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
@@ -1830,6 +1833,16 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p, const struct cpuma
 static inline bool cpupri_check_rt(void)
 {
 	return false;
+}
+static inline int dup_user_cpus_ptr(struct task_struct *dst, struct task_struct *src, int node)
+{
+	if (src->user_cpus_ptr)
+		return -EINVAL;
+	return 0;
+}
+static inline void release_user_cpus_ptr(struct task_struct *p)
+{
+	BUG_ON(p->user_cpus_ptr);
 }
 #endif
 
