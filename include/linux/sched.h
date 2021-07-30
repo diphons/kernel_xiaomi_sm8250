bba35bc49494b5a332d38a593521b7c0f443b521
Will Deacon <will@kernel.org>
2021-07-30 12:24:33 +0100

    sched: Introduce task_struct::user_cpus_ptr to track requested affinity
    In preparation for saving and restoring the user-requested CPU affinity
    mask of a task, add a new cpumask_t pointer to 'struct task_struct'.
    
    If the pointer is non-NULL, then the mask is copied across fork() and
    freed on task exit.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <Valentin.Schneider@arm.com>
    Link: https://lore.kernel.org/r/20210730112443.23245-7-will@kernel.org

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -844,6 +844,7 @@ struct task_struct {
 	unsigned long			max_allowed_capacity;
 	int				nr_cpus_allowed;
 	const cpumask_t			*cpus_ptr;
+	cpumask_t			*user_cpus_ptr;
 	cpumask_t			cpus_mask;
 	cpumask_t			cpus_requested;
 	void				*migration_pending;
@@ -1792,6 +1793,8 @@ extern int task_can_attach(struct task_struct *p, const struct cpumask *cs_cpus_
 #ifdef CONFIG_SMP
 extern void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask);
 extern int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask);
+extern int dup_user_cpus_ptr(struct task_struct *dst, struct task_struct *src, int node);
+extern void release_user_cpus_ptr(struct task_struct *p);
 extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
@@ -1802,6 +1805,16 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p, const struct cpuma
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
+}
+static inline int dup_user_cpus_ptr(struct task_struct *dst, struct task_struct *src, int node)
+{
+	if (src->user_cpus_ptr)
+		return -EINVAL;
+	return 0;
+}
+static inline void release_user_cpus_ptr(struct task_struct *p)
+{
+	WARN_ON(p->user_cpus_ptr);
 }
 static inline bool cpupri_check_rt(void)
 {
--- kernel/fork.c
+++ kernel/fork.c
@@ -411,6 +411,7 @@ void put_task_stack(struct task_struct *tsk)
 
 void free_task(struct task_struct *tsk)
 {
+	release_user_cpus_ptr(tsk);
 	cpufreq_task_times_exit(tsk);
 	scs_release(tsk);
 
@@ -905,6 +906,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 #ifdef CONFIG_STACKPROTECTOR
 	tsk->stack_canary = get_random_canary();
 #endif
+	dup_user_cpus_ptr(tsk, orig, node);
 
 	/*
 	 * One for the user space visible state that goes away when reaped.
