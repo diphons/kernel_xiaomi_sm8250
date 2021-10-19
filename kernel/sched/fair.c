5d9f433ecedb01a15343a01fb940f17f0e706a43
Vincent Guittot <vincent.guittot@linaro.org>
2021-10-19 14:35:37 +0200

    UPSTREAM: sched/fair: Cleanup newidle_balance
    update_next_balance() uses sd->last_balance which is not modified by
    load_balance() so we can merge the 2 calls in one place.
    
    No functional change
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Link: https://lore.kernel.org/r/20211019123537.17146-6-vincent.guittot@linaro.org
    (cherry picked from commit 8ea9183db4ad8afbcb7089a77c23eaf965b0cacd)
    
    BUG=b:168337303
    TEST=Build and boot
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: I204f10287ce8790408f3da0848c8f03f9e3f0df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3319561
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>
    (cherry picked from commit 46e7fb1968d2e322a9206aa17080027130e5b295)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3339376

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12210,10 +12210,10 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 		int continue_balancing = 1;
 		u64 domain_cost;
 
-		if (avg_idle < curr_cost + sd->max_newidle_lb_cost) {
-			update_next_balance(sd, &next_balance);
+		update_next_balance(sd, &next_balance);
+
+		if (avg_idle < curr_cost + sd->max_newidle_lb_cost)
 			break;
-		}
 
 		if (sd->flags & SD_BALANCE_NEWIDLE) {
 
@@ -12228,8 +12228,6 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 			curr_cost += domain_cost;
 			t0 = t1;
 		}
-
-		update_next_balance(sd, &next_balance);
 
 		/*
 		 * Stop searching for tasks to pull if there are
