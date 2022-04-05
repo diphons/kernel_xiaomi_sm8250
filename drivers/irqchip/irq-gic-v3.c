c3fbb1b0b4c65fa2b44b399423dc0bf443efe516
EmanuelCN <emanuelghub@gmail.com>
2022-04-05 20:23:54 +0200

    qcom/soc and kernel/power commits
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -418,6 +418,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		log_irq_wakeup_reason(irq);
 #ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
 		do {
 			int platform_id = get_cached_platform_id();
