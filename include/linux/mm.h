3b6ddc85bb543998d682054f5d7b7205caa30040
Reinazhard <reinazhard@gmail.com>
2022-04-11 12:15:04 +0200

    Revert "mm: process_reclaim: support reclaim of driver owned pages"
    This reverts commit 055a204262025addffa1efe66022560f09e740ce.
    
    Change-Id: Ib47d6d829b4b8a9e7d5bab568d70980bf205d1b1
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3043,12 +3043,6 @@ extern struct reclaim_param reclaim_task_anon(struct task_struct *task,
 		int nr_to_reclaim);
 extern int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
 				unsigned long end, struct mm_walk *walk);
-extern struct reclaim_param reclaim_task_nomap(struct task_struct *task,
-		int nr_to_reclaim);
-extern int reclaim_address_space(struct address_space *mapping,
-		struct reclaim_param *rp, struct vm_area_struct *vma);
-extern int proc_reclaim_notifier_register(struct notifier_block *nb);
-extern int proc_reclaim_notifier_unregister(struct notifier_block *nb);
 #endif
 
 #endif /* __KERNEL__ */
