efa1033c3953c61bf46848f021a57fbc83ac960d
Divyanshu-Modi <divyan.m05@gmail.com>
2022-08-02 15:17:43 +0530

    drivers: disable debugfs file creation when !CONFIG_DEBUG_FS
    Change-Id: I4e483e4e74a306ac9213887950eeaf4ce1165947
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -180,8 +180,10 @@ enum fastrpc_remote_subsys_state {
 static int fastrpc_pdr_notifier_cb(struct notifier_block *nb,
 					unsigned long code,
 					void *data);
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *debugfs_root;
 static struct dentry *debugfs_global_file;
+#endif
 
 static inline void mem_barrier(void)
 {
@@ -4310,6 +4312,7 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 	cur_comm[TASK_COMM_LEN-1] = '\0';
 	fl->tgid = current->tgid;
 	snprintf(strpid, PID_SIZE, "%d", current->pid);
+#ifdef CONFIG_DEBUG_FS
 	if (debugfs_root) {
 		buf_size = strlen(cur_comm) + strlen("_")
 			+ strlen(strpid) + 1;
@@ -4339,6 +4342,7 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 			fl->debug_buf = NULL;
 		}
 	}
+#endif
 	return err;
 }
 
@@ -5070,6 +5074,7 @@ static int fastrpc_cb_probe(struct device *dev)
 	}
 
 	chan->sesscount++;
+#ifdef CONFIG_DEBUG_FS
 	if (debugfs_root) {
 		debugfs_global_file = debugfs_create_file("global", 0644,
 			debugfs_root, NULL, &debugfs_fops);
@@ -5079,6 +5084,7 @@ static int fastrpc_cb_probe(struct device *dev)
 			debugfs_global_file = NULL;
 		}
 	}
+#endif
 bail:
 	return err;
 }
@@ -5613,7 +5619,9 @@ static void __exit fastrpc_device_exit(void)
 		wakeup_source_unregister(me->wake_source);
 	if (me->wake_source_secure)
 		wakeup_source_unregister(me->wake_source_secure);
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(debugfs_root);
+#endif
 }
 
 late_initcall(fastrpc_device_init);
