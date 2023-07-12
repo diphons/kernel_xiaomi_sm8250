9ee72964aa427404c52958f1ee05f67ac3bdc979
Phil Auld <pauld@redhat.com>
2023-07-12 09:33:57 -0400

    sched/fair: Block nohz tick_stop when cfs bandwidth in use
    CFS bandwidth limits and NOHZ full don't play well together.  Tasks
    can easily run well past their quotas before a remote tick does
    accounting.  This leads to long, multi-period stalls before such
    tasks can run again. Currently, when presented with these conflicting
    requirements the scheduler is favoring nohz_full and letting the tick
    be stopped. However, nohz tick stopping is already best-effort, there
    are a number of conditions that can prevent it, whereas cfs runtime
    bandwidth is expected to be enforced.
    
    Make the scheduler favor bandwidth over stopping the tick by setting
    TICK_DEP_BIT_SCHED when the only running task is a cfs task with
    runtime limit enabled. We use cfs_b->hierarchical_quota to
    determine if the task requires the tick.
    
    Add check in pick_next_task_fair() as well since that is where
    we have a handle on the task that is actually going to be running.
    
    Add check in sched_can_stop_tick() to cover some edge cases such
    as nr_running going from 2->1 and the 1 remains the running task.
    
    Reviewed-By: Ben Segall <bsegall@google.com>
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20230712133357.381137-3-pauld@redhat.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -503,6 +503,7 @@ extern void init_cfs_bandwidth(struct cfs_bandwidth *cfs_b, struct cfs_bandwidth
 extern void __refill_cfs_bandwidth_runtime(struct cfs_bandwidth *cfs_b);
 extern void start_cfs_bandwidth(struct cfs_bandwidth *cfs_b);
 extern void unthrottle_cfs_rq(struct cfs_rq *cfs_rq);
+extern bool cfs_task_bw_constrained(struct task_struct *p);
 
 extern void unregister_rt_sched_group(struct task_group *tg);
 extern void free_rt_sched_group(struct task_group *tg);
@@ -541,6 +542,7 @@ static inline void set_task_rq_fair(struct sched_entity *se,
 #else /* CONFIG_CGROUP_SCHED */
 
 struct cfs_bandwidth { };
+static inline bool cfs_task_bw_constrained(struct task_struct *p) { return false; }
 
 #endif	/* CONFIG_CGROUP_SCHED */
 
