3306fefa576a4e9e8cbe0b0ff2d0a3e64e872460
Vincent Guittot <vincent.guittot@linaro.org>
2023-12-01 17:16:52 +0100

    sched/fair: Simplify util_est
    With UTIL_EST_FASTUP now being permanent, we can take advantage of the
    fact that the ewma jumps directly to a higher utilization at dequeue to
    simplify util_est and remove the enqueued field.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Hongyan Xia <hongyan.xia2@arm.com>
    Reviewed-by: Alex Shi <alexs@kernel.org>
    Link: https://lore.kernel.org/r/20231201161652.1241695-3-vincent.guittot@linaro.org

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2210,7 +2210,7 @@ static inline unsigned long __cpu_util(int cpu)
 	util = READ_ONCE(cfs_rq->avg.util_avg);
 
 	if (sched_feat(UTIL_EST))
-		util = max(util, READ_ONCE(cfs_rq->avg.util_est.enqueued));
+		util = max(util, READ_ONCE(cfs_rq->avg.util_est));
 
 	return min_t(unsigned long, util, capacity_orig_of(cpu));
 }
