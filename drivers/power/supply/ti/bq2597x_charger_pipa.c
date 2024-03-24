d9f75f847df095fa4e197904cfaf1603602bc42f
Kazuki H <kazukih0205@gmail.com>
2024-03-24 18:20:27 +0900

    bq2597x: pipa: Remove floating point arithmetics
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger_pipa.c
+++ drivers/power/supply/ti/bq2597x_charger_pipa.c
@@ -35,7 +35,6 @@
 #include <linux/debugfs.h>
 #include <linux/bitops.h>
 #include <linux/math64.h>
-#include <asm/neon.h>
 #include "bq25970_reg.h"
 /*#include "bq2597x.h"*/
 
@@ -58,7 +57,7 @@ enum {
 	ADC_MAX_NUM,
 };
 
-static float sc8551_adc_lsb[] = {
+static int sc8551_adc_lsb[] = {
 	[ADC_IBUS]	= SC8551_IBUS_ADC_LSB,
 	[ADC_VBUS]	= SC8551_VBUS_ADC_LSB,
 	[ADC_VAC]	= SC8551_VAC_ADC_LSB,
@@ -1109,7 +1108,7 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel,  int *result)
 		*result = t;
 		/* vbat need calibration read by NU2105 */
 		if (channel == ADC_VBAT) {
-			t = t * (NU2105_SCALE_FACTOR + NU2105_CALIBRATION_FACTOR) / NU2105_SCALE_FACTOR;
+			t = t * (1 + 1803 / 1000000);
 			*result = t;
 		}
 	} else {
@@ -1122,7 +1121,7 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel,  int *result)
 		*result = t;
 
 		if (bq->chip_vendor == SC8551)
-			*result = (int)(t * sc8551_adc_lsb[channel]);
+			*result = (u64)t * (u64)sc8551_adc_lsb[channel] / 10000000;
 	}
 
 	return 0;
