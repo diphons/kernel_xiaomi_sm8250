e87a9421721c88d68bbd617465d14b04b73a7eff
EmanuelCN <emanuelghub@gmail.com>
2024-04-06 00:07:36 +0300

    kernel: schedutil: Tune rate limits

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1321,19 +1321,19 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us = 5000;
-		tunables->down_rate_limit_us = 5000;
+		tunables->up_rate_limit_us = 500;
+		tunables->down_rate_limit_us = 1000;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 16000;
-		tunables->down_rate_limit_us = 4000;
+		tunables->up_rate_limit_us = 500;
+		tunables->down_rate_limit_us = 1000;
 	}
 
-        if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-                tunables->up_rate_limit_us = 16000;
-                tunables->down_rate_limit_us = 4000;
-        }
+	if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
+		tunables->up_rate_limit_us = 500;
+		tunables->down_rate_limit_us = 1000;
+	}
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
