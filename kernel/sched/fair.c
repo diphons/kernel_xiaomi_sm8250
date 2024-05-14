995be2883fbe76b6f96fe8aa7f8adc5f375fde3a
EmanuelCN <emanuelghub@gmail.com>
2024-05-14 22:25:58 +0300

    kernel/sched: Kang task_fits_capacity from msm-5.4

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4005,8 +4005,7 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	unsigned int margin;
 
 	/*
-	 * Derive upmigration/downmigrate margin wrt the src/dest
-	 * CPU.
+	 * Derive upmigration/downmigrate margin wrt the src/dest CPU.
 	 */
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
 		margin = sched_capacity_margin_down[cpu];
@@ -4029,6 +4028,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 		if (task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
 			task_boost > 0 ||
 			schedtune_task_boost(p) > 0 ||
+			uclamp_boosted(p) ||
 			walt_should_kick_upmigrate(p, cpu))
 			return false;
 	} else { /* mid cap cpu */
