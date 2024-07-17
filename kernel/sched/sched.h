37c2efbff5e7bcda57870639e40d3d2e94464c74
Chuyi Zhou <zhouchuyi@bytedance.com>
2024-07-17 22:33:42 +0800

    sched/fair: Remove cfs_rq::nr_spread_over and cfs_rq::exec_clock
    nr_spread_over tracks the number of instances where the difference
    between a scheduling entity's virtual runtime and the minimum virtual
    runtime in the runqueue exceeds three times the scheduler latency,
    indicating significant disparity in task scheduling.
    Commit that removed its usage: 5e963f2bd: sched/fair: Commit to EEVDF
    
    cfs_rq->exec_clock was used to account for time spent executing tasks.
    Commit that removed its usage: 5d69eca542ee1 sched: Unify runtime
    accounting across classes
    
    cfs_rq::nr_spread_over and cfs_rq::exec_clock are not used anymore in
    eevdf. Remove them from struct cfs_rq.
    
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Chengming Zhou <chengming.zhou@linux.dev>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Acked-by: Vishal Chourasia <vishalc@linux.ibm.com>
    Link: https://lore.kernel.org/r/20240717143342.593262-1-zhouchuyi@bytedance.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -570,7 +570,6 @@ struct cfs_rq {
 	s64			avg_vruntime;
 	u64			avg_load;
 
-	u64			exec_clock;
 	u64			min_vruntime;
 
 	struct rb_root_cached	tasks_timeline;
@@ -581,10 +580,6 @@ struct cfs_rq {
 	 */
 	struct sched_entity	*curr;
 	struct sched_entity	*next;
-
-#ifdef	CONFIG_SCHED_DEBUG
-	unsigned int		nr_spread_over;
-#endif
 
 #ifdef CONFIG_SMP
 	/*
@@ -1105,7 +1100,6 @@ struct rq {
 	/* latency stats */
 	struct sched_info	rq_sched_info;
 	unsigned long long	rq_cpu_time;
-	/* could above be rq->cfs_rq.exec_clock + rq->rt_rq.rt_runtime ? */
 
 	/* sys_sched_yield() stats */
 	unsigned int		yld_count;
