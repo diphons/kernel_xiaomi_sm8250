90b3411c5e7af7e8599bfcb43ea0017faa5c1f0f
amackpro <prajwal.kumar008@gmail.com>
2024-07-23 19:54:14 +0530

    drivers: oplus: Update wakelock profile

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -296,6 +296,12 @@ static void print_wakeup_sources(void)
 	spin_unlock_irqrestore(&wakeup_reason_lock, flags);
 }
 
+#ifdef CONFIG_OPLUS
+extern void bts_net_clear(void);
+extern bool bts_net_exist(void);
+extern ssize_t bts_net_fill(char * desc, ssize_t size);
+#endif /* CONFIG_OPLUS */
+
 static ssize_t last_resume_reason_show(struct kobject *kobj,
 				       struct kobj_attribute *attr, char *buf)
 {
@@ -312,11 +318,23 @@ static ssize_t last_resume_reason_show(struct kobject *kobj,
 		return buf_offset;
 	}
 
+#ifdef CONFIG_OPLUS
+	if(bts_net_exist()) {
+		buf_offset +=  bts_net_fill(buf + buf_offset, PAGE_SIZE - buf_offset);
+		bts_net_clear();
+	}
+#endif /* CONFIG_OPLUS */
+
 	if (wakeup_reason == RESUME_IRQ && !list_empty(&leaf_irqs))
 		list_for_each_entry(n, &leaf_irqs, siblings)
 			buf_offset += scnprintf(buf + buf_offset,
 						PAGE_SIZE - buf_offset,
+#ifdef CONFIG_OPLUS
+						"%d %s\n", n->irq,
+						(strncmp(n->irq_name, "ipcc_1", strlen("ipcc_1")) == 0) ? "qmi" : n->irq_name);
+#else
 						"%d %s\n", n->irq, n->irq_name);
+#endif /* CONFIG_OPLUS */
 	else if (wakeup_reason == RESUME_ABNORMAL)
 		buf_offset = scnprintf(buf, PAGE_SIZE, "-1 %s",
 				       non_irq_wake_reason);
