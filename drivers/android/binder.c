99322db19435519c8826d39e78554175f9cc0188
Carlos Llamas <cmllamas@google.com>
2024-07-25 18:13:35 +0000

    ANDROID: binder: fix KMI-break due to proc->dmap
    Add 'struct binder_proc_wrap' to support the addition of new members in
    'struct binder_proc' without breaking the KMI. In this case, proc->dmap
    was backported from upstream and needs to be migrated into this wrapper.
    
    Avoids the following KMI issue:
    
      function symbol 'int __traceiter_binder_transaction_received(void*, struct binder_transaction*)' changed
        CRC changed from 0x74e9c98b to 0x7af6cf5a
    
      type 'struct binder_proc' changed
        byte size changed from 584 to 600
        member 'struct dbitmap dmap' was added
        16 members ('struct list_head todo' .. 'u64 android_oem_data1') changed
          offset changed by 128
    
    Bug: 298520209
    Change-Id: Icbbee14a8f16d0881faf8d5673582e785f98e8cf
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    (cherry picked from commit af55892f201c8f709725d75d306b1cdd20984b97)
    [cmllamas: merge binder_proc_wrap_entry() with new proc_wrapper()]
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1098,7 +1098,7 @@ static int get_ref_desc_olocked(struct binder_proc *proc,
 				struct binder_node *node,
 				u32 *desc)
 {
-	struct dbitmap *dmap = &proc->dmap;
+	struct dbitmap *dmap = &proc_wrapper(proc)->dmap;
 	unsigned int nbits, offset;
 	unsigned long *new, bit;
 
@@ -1207,7 +1207,7 @@ retry:
 }
 static void binder_cleanup_ref_olocked(struct binder_ref *ref)
 {
-	struct dbitmap *dmap = &ref->proc->dmap;
+	struct dbitmap *dmap = &proc_wrapper(ref->proc)->dmap;
 	bool delete_node = false;
 	binder_debug(BINDER_DEBUG_INTERNAL_REFS,
 		     "%d delete ref %d desc %d for node %d\n",
@@ -1372,11 +1372,12 @@ static void binder_free_ref(struct binder_ref *ref)
 /* shrink descriptor bitmap if needed */
 static void try_shrink_dmap(struct binder_proc *proc)
 {
+	struct dbitmap *dmap = &proc_wrapper(proc)->dmap;
 	unsigned long *new;
 	int nbits;
 
 	binder_proc_lock(proc);
-	nbits = dbitmap_shrink_nbits(&proc->dmap);
+	nbits = dbitmap_shrink_nbits(dmap);
 	binder_proc_unlock(proc);
 
 	if (!nbits)
@@ -1384,7 +1385,7 @@ static void try_shrink_dmap(struct binder_proc *proc)
 
 	new = bitmap_zalloc(nbits, GFP_KERNEL);
 	binder_proc_lock(proc);
-	dbitmap_shrink(&proc->dmap, new, nbits);
+	dbitmap_shrink(dmap, new, nbits);
 	binder_proc_unlock(proc);
 }
 
@@ -4660,7 +4661,6 @@ static struct binder_thread *binder_get_thread(struct binder_proc *proc)
 }
 static void binder_free_proc(struct binder_proc *proc)
 {
-	struct binder_proc_wrap *proc_wrap;
 	struct binder_device *device;
 
 	BUG_ON(!list_empty(&proc->todo));
@@ -4677,9 +4677,8 @@ static void binder_free_proc(struct binder_proc *proc)
 	put_task_struct(proc->tsk);
 	put_cred(proc->cred);
 	binder_stats_deleted(BINDER_STAT_PROC);
-	dbitmap_free(&proc->dmap);
-	proc_wrap = binder_proc_wrap_entry(proc);
-	kfree(proc_wrap);
+	dbitmap_free(&proc_wrapper(proc)->dmap);
+	kfree(proc_wrapper(proc));
 }
 static void binder_free_thread(struct binder_thread *thread)
 {
@@ -5327,7 +5326,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 		return -ENOMEM;
 	proc = &proc_wrap->proc;
 
-	dbitmap_init(&proc->dmap);
+	dbitmap_init(&proc_wrapper(proc)->dmap);
 	spin_lock_init(&proc->inner_lock);
 	spin_lock_init(&proc->outer_lock);
 	get_task_struct(current->group_leader);
