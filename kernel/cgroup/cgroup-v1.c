7a328d990aa1be79297c6d3131c2cf05bd60315e
Rudi Setiyawan <diphons@gmail.com>
2024-07-31 08:31:38 +0700

    kp: drop no need this Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -509,7 +509,6 @@ static int cgroup_pidlist_show(struct seq_file *s, void *v)
 	return 0;
 }
 
-extern int kp_active_mode(void);
 static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 				     char *buf, size_t nbytes, loff_t off,
 				     bool threadgroup)
@@ -550,11 +549,7 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	if (!ret && !threadgroup &&
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		task_is_zygote(task->parent)) {
-		if (kp_active_mode() == 3 || kp_active_mode() == 0) {
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 500);
-		} else if (kp_active_mode() == 2) {
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 250);
-		}
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 500);
 	}
 
 out_finish:
--- kernel/fork.c
+++ kernel/fork.c
@@ -2344,17 +2344,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost CPU to the max for 100 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-	  /*
-	   * Dont boost CPU & DDR if battery saver profile is enabled
-	   * and boost CPU & DDR for 25ms if balanced profile is enabled
-	   */
-	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
-	    devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 100);
-	  } else if (kp_active_mode() == 2) {
-	    devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 50);
-	  }
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 100);
 	}
 
 	/*
