5f324528d837109368899c645596898eaf962ad1
Rudi Setiyawan <diphons@gmail.com>
2024-08-01 23:58:11 +0700

    power: maxim: ds28e16: pipa: Silent spam log
    also silent Setting cpu affinity
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/power/supply/maxim_pipa/ds28e16_master.c
+++ drivers/power/supply/maxim_pipa/ds28e16_master.c
@@ -164,7 +164,7 @@ short Read_RomID(unsigned char *RomID)
 	for (i = 0; i < 8; i++)
 		RomID[i] = read_byte();
 
-	ds_info("RomID = %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x\n",
+	ds_dbg("RomID = %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x\n",
 	RomID[0], RomID[1], RomID[2], RomID[3],
 	RomID[4], RomID[5], RomID[6], RomID[7]);
 
@@ -1677,16 +1677,16 @@ static void battery_verify(struct work_struct *work)
 
 	if (result == DS_TRUE) {
 		data->batt_verified = 1;
-		ds_info("%s batt_verified = 1 \n", __func__);
+		ds_dbg("%s batt_verified = 1 \n", __func__);
 	} else {
 		data->batt_verified = 0;
 		if (count < VERIFY_MAX_COUNT) {
 			schedule_delayed_work(&data->battery_verify_work,
 						msecs_to_jiffies(VERIFY_PERIOD_S));
-			ds_info("%s battery verify failed times[%d]", __func__, count);
+			ds_dbg("%s battery verify failed times[%d]", __func__, count);
 			count++;
 		} else {
-			ds_info("%s battery verify failed[%d]", __func__, result);
+			ds_dbg("%s battery verify failed[%d]", __func__, result);
 		}
 	}
 }
