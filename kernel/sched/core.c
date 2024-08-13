4638c225ba3330c9b61f9206dc27ce3c5d278bd9
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:52 +0200

    sched: Split up put_prev_task_balance()
    With the goal of pushing put_prev_task() after pick_task() / into
    pick_next_task().
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224015.943143811@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5187,8 +5187,8 @@ static inline void schedule_debug(struct task_struct *prev)
 	schedstat_inc(this_rq()->sched_count);
 }
 
-static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
-				  struct rq_flags *rf)
+static void prev_balance(struct rq *rq, struct task_struct *prev,
+			 struct rq_flags *rf)
 {
 #ifdef CONFIG_SMP
 	const struct sched_class *class;
@@ -5205,8 +5205,6 @@ static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
 			break;
 	}
 #endif
-
-	put_prev_task(rq, prev);
 
 	/*
 	 * We've updated @prev and no longer need the server link, clear it.
@@ -5264,7 +5262,8 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	}
 
 restart:
-	put_prev_task_balance(rq, prev, rf);
+	prev_balance(rq, prev, rf);
+	put_prev_task(rq, prev);
 
 	for_each_class(class) {
 		p = class->pick_next_task(rq);
