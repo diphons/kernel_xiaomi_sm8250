83048445120a64e9b9974741bfc391d43c61f80b
Rudi Setiyawan <diphons@gmail.com>
2024-08-22 12:54:09 +0700

    Import Oplus Feature - oproject - oext4 - nandswap - uxio first - wakelock profile - sleep monitor - nwpower Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -16,6 +16,9 @@
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
 #include <linux/suspend.h>
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+#include <linux/rtc.h>
+#endif /* CONFIG_OPLUS_WAKELOCK_PROFILER */
 
 #include "power.h"
 
@@ -601,6 +604,10 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 	suspend_state_t state;
 	int error;
 
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("PM: enter state_store, buf=%s.\n", buf);
+#endif /* CONFIG_OPLUS_WAKELOCK_PROFILER */
+
 	error = pm_autosleep_lock();
 	if (error)
 		return error;
@@ -658,6 +665,32 @@ power_attr(state);
  * are any wakeup events detected after 'wakeup_count' was written to.
  */
 
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+static void pm_wakeup_count_marker(char *annotation)
+{
+	struct timespec ts;
+	struct rtc_time tm;
+
+	getnstimeofday(&ts);
+	rtc_time_to_tm(ts.tv_sec, &tm);
+	pr_info("PM: wakeup_count %s %d-%02d-%02d %02d:%02d:%02d.%09lu UTC\n",
+		annotation, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+		tm.tm_hour, tm.tm_min, tm.tm_sec, ts.tv_nsec);
+}
+
+static ssize_t wakeup_count_show(struct kobject *kobj,
+				struct kobj_attribute *attr,
+				char *buf)
+{
+	unsigned int val, error;
+
+	pm_wakeup_count_marker("read enter");
+	error = pm_get_wakeup_count(&val, true);
+	pm_wakeup_count_marker("read exit");
+
+	return error ? sprintf(buf, "%u\n", val) : -EINTR;
+}
+#else
 static ssize_t wakeup_count_show(struct kobject *kobj,
 				struct kobj_attribute *attr,
 				char *buf)
@@ -667,6 +700,7 @@ static ssize_t wakeup_count_show(struct kobject *kobj,
 	return pm_get_wakeup_count(&val, true) ?
 		sprintf(buf, "%u\n", val) : -EINTR;
 }
+#endif /* CONFIG_OPLUS_WAKELOCK_PROFILER */
 
 static ssize_t wakeup_count_store(struct kobject *kobj,
 				struct kobj_attribute *attr,
@@ -674,6 +708,10 @@ static ssize_t wakeup_count_store(struct kobject *kobj,
 {
 	unsigned int val;
 	int error;
+
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pm_wakeup_count_marker("store");
+#endif /* CONFIG_OPLUS_WAKELOCK_PROFILER */
 
 	error = pm_autosleep_lock();
 	if (error)
