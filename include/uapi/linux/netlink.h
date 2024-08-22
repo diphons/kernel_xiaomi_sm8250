9c86047034700f87a7d30ee0944135d5910cd835
Rudi Setiyawan <diphons@gmail.com>
2024-08-22 12:54:09 +0700

    Import Oplus Feature - oproject - oext4 - nandswap - uxio first - wakelock profile - sleep monitor - nwpower Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- include/uapi/linux/netlink.h
+++ include/uapi/linux/netlink.h
@@ -33,7 +33,13 @@
 #define NETLINK_INET_DIAG	NETLINK_SOCK_DIAG
 #define NETLINK_MILLET          29
 
-#define MAX_LINKS 32		
+#ifdef CONFIG_OPLUS
+#define NETLINK_OPLUS_NWPOWERSTATE	32	/*OPLUS NW PowerState*/
+#define NETLINK_OPLUS_IPV6_RTO  33
+#define MAX_LINKS 34
+#else
+#define MAX_LINKS 32
+#endif /* CONFIG_OPLUS */
 
 struct sockaddr_nl {
 	__kernel_sa_family_t	nl_family;	/* AF_NETLINK	*/
