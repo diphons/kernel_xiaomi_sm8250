367169a3db0334ce4f8b23f6dfe21e1d4e3c2fdf
Rudi Setiyawan <diphons@gmail.com>
2024-08-22 12:54:09 +0700

    Import Oplus Feature - oproject - oext4 - nandswap - uxio first - wakelock profile - sleep monitor - nwpower Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -45,6 +45,10 @@
 
 #include <trace/events/tcp.h>
 
+#ifdef CONFIG_OPLUS
+#include <oplus/oplus_nwpower.h>
+#endif /* CONFIG_OPLUS */
+
 /* Refresh clocks of a TCP socket,
  * ensuring monotically increasing values.
  */
@@ -1321,6 +1325,10 @@ static int __tcp_transmit_skb(struct sock *sk, struct sk_buff *skb,
 			       sizeof(struct inet6_skb_parm)));
 
 	err = icsk->icsk_af_ops->queue_xmit(sk, skb, &inet->cork.fl);
+
+#ifdef CONFIG_OPLUS
+	oplus_match_tcp_output(sk);
+#endif /* CONFIG_OPLUS */
 
 	if (unlikely(err > 0)) {
 		tcp_enter_cwr(sk);
@@ -3168,6 +3176,9 @@ start:
 
 	if (likely(!err)) {
 		TCP_SKB_CB(skb)->sacked |= TCPCB_EVER_RETRANS;
+#ifdef CONFIG_OPLUS
+		oplus_match_tcp_output_retrans(sk);
+#endif /* CONFIG_OPLUS */
 		trace_tcp_retransmit_skb(sk, skb);
 	} else if (err != -EBUSY) {
 		NET_ADD_STATS(sock_net(sk), LINUX_MIB_TCPRETRANSFAIL, segs);
