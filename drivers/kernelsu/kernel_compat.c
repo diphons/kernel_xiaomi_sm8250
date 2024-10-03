4c35f3f130ec997ca124a8fa317439c660153676
Rudi Setiyawan <diphons@gmail.com>
2024-10-03 09:52:31 +0700

    kernelsu: kernel_compat: use new argument access_ok() Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/kernelsu/kernel_compat.c
+++ drivers/kernelsu/kernel_compat.c
@@ -49,13 +49,8 @@ void ksu_android_ns_fs_check()
 }
 
 int ksu_access_ok(const void *addr, unsigned long size) {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
-    /* For kernels before 5.0.0, pass the type argument to access_ok. */
-    return access_ok(VERIFY_READ, addr, size);
-#else
     /* For kernels 5.0.0 and later, ignore the type argument. */
     return access_ok(addr, size);
-#endif
 }
 
 struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
