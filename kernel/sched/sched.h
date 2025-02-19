45a224f653077afdeb6cb5d4d7dc336a05875a33
Xuewen Yan <xuewen.yan@unisoc.com>
2025-02-19 17:37:47 +0800

    sched/uclamp: Optimize sched_uclamp_used static key enabling
    Repeat calls of static_branch_enable() to an already enabled
    static key introduce overhead, because it calls cpus_read_lock().
    
    Users may frequently set the uclamp value of tasks, triggering
    the repeat enabling of the sched_uclamp_used static key.
    
    Optimize this and avoid repeat calls to static_branch_enable()
    by checking whether it's enabled already.
    
    [ mingo: Rewrote the changelog for legibility ]
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20250219093747.2612-2-xuewen.yan@unisoc.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2524,6 +2524,18 @@ static inline bool uclamp_is_used(void)
 	return static_branch_likely(&sched_uclamp_used);
 }
 
+/*
+ * Enabling static branches would get the cpus_read_lock(),
+ * check whether uclamp_is_used before enable it to avoid always
+ * calling cpus_read_lock(). Because we never disable this
+ * static key once enable it.
+ */
+static inline void sched_uclamp_enable(void)
+{
+	if (!uclamp_is_used())
+		static_branch_enable(&sched_uclamp_used);
+}
+
 static inline unsigned long uclamp_rq_get(struct rq *rq,
 					  enum uclamp_id clamp_id)
 {
@@ -2606,6 +2618,8 @@ static inline bool uclamp_is_used(void)
 {
 	return false;
 }
+
+static inline void sched_uclamp_enable(void) {}
 
 static inline unsigned long uclamp_rq_get(struct rq *rq,
 					  enum uclamp_id clamp_id)
