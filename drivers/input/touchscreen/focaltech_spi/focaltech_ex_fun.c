a0372a459273c6a698157b66ae169b1973f139d2
Rudi Setiyawan <diphons@gmail.com>
2025-02-23 01:41:02 +0700

    touchscreen: focaltech_spi: convert to use "struct proc_ops" Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/input/touchscreen/focaltech_spi/focaltech_ex_fun.c
+++ drivers/input/touchscreen/focaltech_spi/focaltech_ex_fun.c
@@ -278,10 +278,9 @@ proc_read_err:
 	return ret;
 }
 
-static const struct file_operations fts_proc_fops = {
-	.owner  = THIS_MODULE,
-	.read   = fts_debug_read,
-	.write  = fts_debug_write,
+static const struct proc_ops fts_proc_fops = {
+	.proc_read   = fts_debug_read,
+	.proc_write  = fts_debug_write,
 };
 #else
 static int fts_debug_write(
@@ -516,8 +515,8 @@ static ssize_t fts_fw_version_read(struct file *filp,
 		return cnt;
 }
 
-static const struct file_operations tp_fw_version_fops = {
-	.read = fts_fw_version_read,
+static const struct proc_ops tp_fw_version_fops = {
+	.proc_read = fts_fw_version_read,
 };
 
 
@@ -545,8 +544,8 @@ static ssize_t fts_lockdown_info_read(struct file *filp,
 		return cnt;
 }
 
-static const struct file_operations tp_lockdown_info_fops = {
-	.read = fts_lockdown_info_read,
+static const struct proc_ops tp_lockdown_info_fops = {
+	.proc_read = fts_lockdown_info_read,
 };
 
 
