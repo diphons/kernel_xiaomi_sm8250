3597f486b7fedcd1454fb5b9dde33e667230f4ec
Juri Lelli <juri.lelli@redhat.com>
2025-03-13 18:12:02 +0100

    cgroup/cpuset: Remove partition_and_rebuild_sched_domains
    partition_and_rebuild_sched_domains() and partition_sched_domains() are
    now equivalent.
    
    Remove the former as a nice clean up.
    
    Suggested-by: Waiman Long <llong@redhat.com>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Waiman Long <llong@redhat.com>
    Reviewed-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Waiman Long <longman@redhat.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/Z9MR4ryNDJZDzsSG@jlelli-thinkpadt14gen4.remote.csb

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -929,15 +929,6 @@ void dl_rebuild_rd_accounting(void)
 	rcu_read_unlock();
 }
 
-static void
-partition_and_rebuild_sched_domains(int ndoms_new, cpumask_var_t doms_new[],
-				    struct sched_domain_attr *dattr_new)
-{
-	sched_domains_mutex_lock();
-	partition_sched_domains_locked(ndoms_new, doms_new, dattr_new);
-	sched_domains_mutex_unlock();
-}
-
 /*
  * Rebuild scheduler domains.
  *
@@ -970,7 +961,7 @@ static void rebuild_sched_domains_locked(void)
 	ndoms = generate_sched_domains(&doms, &attr);
 
 	/* Have scheduler rebuild the domains */
-	partition_and_rebuild_sched_domains(ndoms, doms, attr);
+	partition_sched_domains(ndoms, doms, attr);
 }
 #else /* !CONFIG_SMP */
 static void rebuild_sched_domains_locked(void)
