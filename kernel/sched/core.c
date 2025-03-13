d3200bc3416b0f4e528f930a6977aa21ffb020d5
Juri Lelli <juri.lelli@redhat.com>
2025-03-13 18:10:21 +0100

    sched/deadline: Rebuild root domain accounting after every update
    Rebuilding of root domains accounting information (total_bw) is
    currently broken on some cases, e.g. suspend/resume on aarch64. Problem
    is that the way we keep track of domain changes and try to add bandwidth
    back is convoluted and fragile.
    
    Fix it by simplify things by making sure bandwidth accounting is cleared
    and completely restored after root domains changes (after root domains
    are again stable).
    
    To be sure we always call dl_rebuild_rd_accounting while holding
    cpuset_mutex we also add cpuset_reset_sched_domains() wrapper.
    
    Fixes: 53916d5fd3c0 ("sched/deadline: Check bandwidth overflow earlier for hotplug")
    Reported-by: Jon Hunter <jonathanh@nvidia.com>
    Co-developed-by: Waiman Long <llong@redhat.com>
    Signed-off-by: Waiman Long <llong@redhat.com>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/Z9MRfeJKJUOyUSto@jlelli-thinkpadt14gen4.remote.csb

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8291,7 +8291,7 @@ static void cpuset_cpu_active(void)
 		 * operation in the resume sequence, just build a single sched
 		 * domain, ignoring cpusets.
 		 */
-		partition_sched_domains(1, NULL, NULL);
+		cpuset_reset_sched_domains();
 		if (--num_cpus_frozen)
 			return;
 		/*
@@ -8310,7 +8310,7 @@ static void cpuset_cpu_inactive(unsigned int cpu)
 		cpuset_update_active_cpus();
 	} else {
 		num_cpus_frozen++;
-		partition_sched_domains(1, NULL, NULL);
+		cpuset_reset_sched_domains();
 	}
 }
 
