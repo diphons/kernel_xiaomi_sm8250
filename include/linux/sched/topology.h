56d046aa0164e14e12bd1918cfbe250d3daf4862
Juri Lelli <juri.lelli@redhat.com>
2025-03-13 18:12:43 +0100

    sched/topology: Stop exposing partition_sched_domains_locked
    The are no callers of partition_sched_domains_locked() outside
    topology.c.
    
    Stop exposing such function.
    
    Suggested-by: Waiman Long <llong@redhat.com>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Waiman Long <longman@redhat.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/Z9MSC96a8FcqWV3G@jlelli-thinkpadt14gen4.remote.csb

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -182,10 +182,6 @@ static inline struct cpumask *sched_domain_span(struct sched_domain *sd)
 
 extern void dl_rebuild_rd_accounting(void);
 
-extern void partition_sched_domains_locked(int ndoms_new,
-					   cpumask_var_t doms_new[],
-					   struct sched_domain_attr *dattr_new);
-
 extern void partition_sched_domains(int ndoms_new, cpumask_var_t doms_new[],
 				    struct sched_domain_attr *dattr_new);
 
@@ -229,12 +225,6 @@ extern void set_sched_topology(struct sched_domain_topology_level *tl);
 #else /* CONFIG_SMP */
 
 struct sched_domain_attr;
-
-static inline void
-partition_sched_domains_locked(int ndoms_new, cpumask_var_t doms_new[],
-			       struct sched_domain_attr *dattr_new)
-{
-}
 
 static inline void
 partition_sched_domains(int ndoms_new, cpumask_var_t doms_new[],
