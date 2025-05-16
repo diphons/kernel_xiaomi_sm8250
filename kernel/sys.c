f6fe0a3e3801572a5be38a3e12c849b28e8b4ad8
Thomas Turner <thomas_turner@talktalk.net>
2025-05-16 21:00:30 +0100

    kernel: Fake uname to 5.4.186
    Needed to boot Android 16 and above on 4.19 devices.
    
    Signed-off-by: waffleowo <featherusuwu@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1272,6 +1272,13 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "5.4.186");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
